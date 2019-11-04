fc983ff868a9c48bfe422fe9e5fb8ebc8994edb2
Leo Liu <leo.liu.tw@pmgi.local>
2019-11-04 17:29:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created () {
+    // api server path + remote path
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

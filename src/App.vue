7cc55f36a55e99cb99ae1a694ba680c3b479a67e
Leo Liu <leo.liu.tw@pmgi.local>
2019-11-06 19:54:03 +0800

    add product functionality

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,7 @@
 <script>
 
 export default {
-  name: 'App',
-  created () {
-    // api server path + remote path
-    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`
-    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
-    this.$http.get(api).then((response) => {
-      console.log(response.data)
-    })
-  }
+  name: 'App'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import 'bootstrap'
+
 import router from './router'
 import App from './App'
 

b48835b0318a8721aac5a1c824deff4baae1f26a
Leo Liu <leo.liu.tw@pmgi.local>
2020-02-21 15:54:00 +0800

    path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './vue-store/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
